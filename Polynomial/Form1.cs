62afcabfa9a29ade207ed23558fe42c7b6e43317
Radmir2015 <ya.radmir2015@yandex.ru>
2018-09-16 21:09:00 +0300

    basic structure & some GUI

--- Polynomial/Form1.cs
+++ Polynomial/Form1.cs
@@ -16,5 +16,41 @@ namespace Polynomial
         {
             InitializeComponent();
         }
+
+        public class Polynom
+        {
+            private List<Node> nodes = new List<Node>();
+
+            public void add(Node node) => nodes.Add(node);
+            public void delete(Node node) => nodes.Remove(node);
+            public void deleteByPower(Node node) => nodes.RemoveAll(x => x.power == node.power);
+
+            public string getRepresentation(string letter = "x") =>
+                string.Join(" + ", nodes.Select(x => x.k.ToString() + letter + "^" + x.power.ToString()));
+        }
+
+        public class Node
+        {
+            public int power = 0;
+            public double k = 0;
+
+            public Node(double k, int power)
+            {
+                this.k = k;
+                this.power = power;
+            }
+        }
+        
+        public int stringToInt(string str) => System.Convert.ToInt32(str);
+
+        Polynom p = new Polynom();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Node n = new Node(stringToInt(textBox1.Text), stringToInt(textBox2.Text));
+            p.add(n);
+
+            textBox3.Text = p.getRepresentation();
+        }
     }
 }
