260906bb6cbe613d65c9c73304b0bb8a65b31cab
Radmir2015 <ya.radmir2015@yandex.ru>
2018-09-20 12:57:30 +0300

    regex parse poly & getrepresent

--- Polynomial/Form1.cs
+++ Polynomial/Form1.cs
@@ -45,12 +45,13 @@ namespace Polynomial
 
             public void Delete(Node node) => nodes.Remove(node);
             public void DeleteByPower(Node node) => nodes.RemoveAll(x => x.GetPower() == node.GetPower());
+            public void Clear() => nodes.Clear();
 
             public string GetRepresentation(string letter = "x") =>
                 string.Join(" + ", nodes.Select(x =>
-                    x.GetK().ToString() +
+                    x.GetK() == 1 ? "" : x.GetK().ToString() +
                     ((x.GetPower() != 0)
-                    ? letter + "^" + x.GetPower().ToString()
+                    ? letter + (x.GetPower() == 1 ? "" : ("^" + x.GetPower().ToString()))
                     : ""))).Replace("+ -", "- ").Replace(",", ".");
 
             public bool IsEmpty() => nodes.Count() == 0;
@@ -150,10 +151,33 @@ namespace Polynomial
         {
             string[] tests = { "3x^2+5  *x^2 -6", "-7x", "-2 * x ^ 4 - 17x^2  " };
             var first = tests[0].Split(new char[] { '+', '-' }).Select(x => x.Trim()).ToArray();
+            first = textBox3.Text.Split(new char[] { '+', '-' }).Select(x => x.Trim()).ToArray();
+
+            //first = Regex.Split(textBox3.Text.Replace("-", " -").Replace("+", " +"), @"\w*(?=[\+-])").Select(x => x.Trim()).ToArray();
+
+            MessageBox.Show(Regex.Replace(textBox3.Text, @"(?<=[0-9a-zA-Z]+)([\+-])", " $1"));
+
+            first = Regex.Split(Regex.Replace(textBox3.Text, @"(?<=[0-9a-zA-Z]+)([\+-])", " $1"), @"\s+(?=[\+-])").Select(x => x.Trim()).ToArray();
+            MessageBox.Show(string.Join(";", first));
+
+            p.Clear();
             //Console.WriteLine(string.Join(";", ));
-            Regex regex = new Regex(@"(\d*)\*?([a-zA-Z]?)\^?(\d*)");
-            Match match = regex.Match(first[1]);
-            textBox5.Text = match.Groups[1].Value + " " + match.Groups[2].Value + " " + match.Groups[3].Value;
+            //Regex regex = new Regex(@"(\d*)\s*\*?\s*([a-zA-Z]?)\s*\^?\s*(\d*)");
+            Regex regex = new Regex(@"([\+-]?\d*)\s*\*?\s*([a-zA-Z]?)\s*\^?\s*([\+-]?\d*)");
+
+            foreach (string node in first)
+            {
+                Match match = regex.Match(node);
+                MessageBox.Show(match.Groups[1].Value + "|" + match.Groups[2].Value + "|" + match.Groups[3].Value);
+                if (match.Groups[1].Value == "" && match.Groups[2].Value == "" || first.Any(x => x == ""))
+                {
+                    MessageBox.Show("Fatal error");
+                    break;
+                }
+                p.Add(new Node((match.Groups[1].Value == "" || match.Groups[1].Value == "+") ? 1 : (match.Groups[1].Value == "-" ? -1 : StringToDouble(match.Groups[1].Value)), match.Groups[3].Value == "" ? (match.Groups[2].Value == "" ? 0 : (match.Groups[2].Value == "-" ? -1 : 1)) : StringToInt(match.Groups[3].Value)));
+            }
+            textBox5.Text = p.GetRepresentation("x"); //match.Groups[1].Value + " " + match.Groups[2].Value + " " + match.Groups[3].Value;
+
         }
     }
 }
